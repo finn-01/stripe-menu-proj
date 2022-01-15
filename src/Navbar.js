bd3df5dfe18bb545370ce32f0afc7e46a433d428
finn-01 <nguyensam576@gmail.com>
2022-01-15 23:21:31 +0700

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,9 +2,57 @@ import React from "react";
 
 import logo from "./images/logo.svg";
 import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Navbar = () => {
-	return <div>Hello Navbar</div>;
+	const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+
+	const displaySubmenu = (e) => {
+		const page = e.target.textContent; //Lay content cua the
+		const tempBtn = e.target.getBoundingClientRect();
+		//console.log(tempBtn);
+		const center = (tempBtn.left + tempBtn.right) / 2;
+		const bottom = tempBtn.bottom - 3;
+		//console.log(center + "-" + bottom);
+		openSubmenu(page, { center, bottom });
+	};
+
+	const handleSubmenu = (e) => {
+		if (e.target.classList.contains("link-btn")) {
+			closeSubmenu();
+		}
+	};
+
+	return (
+		<nav className="nav" onMouseOver={handleSubmenu}>
+			<div className="nav-center">
+				<div className="nav-header">
+					<img src={logo} className="nav-logo" alt="img" />
+					<button className="btn toggle-btn" onClick={openSidebar}>
+						<FaBars />
+					</button>
+				</div>
+				<ul className="nav-links">
+					<li>
+						<button className="link-btn" onMouseOver={displaySubmenu}>
+							products
+						</button>
+					</li>
+					<li>
+						<button className="link-btn" onMouseOver={displaySubmenu}>
+							developers
+						</button>
+					</li>
+					<li>
+						<button className="link-btn" onMouseOver={displaySubmenu}>
+							company
+						</button>
+					</li>
+				</ul>
+				<button className="btn signin-btn">sign in</button>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
